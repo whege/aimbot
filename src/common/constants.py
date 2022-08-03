20080a345d584ff5a19f3ba5f2b32b46a24da3f4
William Hegedusich <whegedusich@gmail.com>
2022-08-02 23:36:45 -0500

    added module for constants, mostly used for permissions strings right now

---
+++ src/common/constants.py
@@ -0,0 +1,10 @@
+__all__ = [
+    "ADMIN_PERMISSION",
+    "VIP_PERMISSION"
+]
+__author__ = """whege"""
+__date__ = "8/2/2022"
+__doc__ = """Enter some text here, bitch"""
+
+ADMIN_PERMISSION = "admin"
+VIP_PERMISSION = "vip"
