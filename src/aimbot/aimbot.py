7e0b14eff8e120c217c837877dd8fe19b8ed0c96
William Hegedusich <whegedusich@gmail.com>
2022-08-02 23:38:03 -0500

    renamed directories so that the NLP portion and chatbot portion could be separated from one another

--- src/aimbot/aimbot.py
+++
@@ -1,9 +0,0 @@
-from twitchbot import BaseBot, Channel
-
-
-class MommyTron(BaseBot):
-    async def on_channel_joined(self, channel: Channel):
-        await channel.send_message("Don't worry, mommy is here! MercyWing1 PinkMercy MercyWing2")
-
-
-MommyTron().run()
