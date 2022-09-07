1aea2d09cbbafe2dc0c9c1e177f98c4ad0730565
William Hegedusich <whegedusich@gmail.com>
2022-09-06 19:50:55 -0500

    added moderator permission

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,5 +1,6 @@
 __all__ = [
     "ADMIN_PERMISSION",
+    "MODERATOR_PERMISSION",
     "VIP_PERMISSION"
 ]
 __author__ = """whege"""
@@ -7,4 +8,5 @@ __date__ = "8/2/2022"
 __doc__ = """Enter some text here, bitch"""
 
 ADMIN_PERMISSION = "admin"
+MODERATOR_PERMISSION = "moderator"
 VIP_PERMISSION = "vip"
