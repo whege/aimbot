47ad7a0e00da24dd7ae6e43f20b542d85c5be650
William Hegedusich <whegedusich@gmail.com>
2022-09-06 19:51:39 -0500

    added __main__.py to run enable running package

---
+++ src/bot/__main__.py
@@ -0,0 +1,9 @@
+__all__ = []
+__author__ = """whege"""
+__date__ = "9/2/2022"
+__doc__ = """Enter some text here, bitch"""
+
+from bot.aimbot import runbot
+
+if __name__ == '__main__':
+    runbot()
